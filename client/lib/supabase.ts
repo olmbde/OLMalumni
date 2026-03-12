5eafd1573c1753afc504485f19a248552966ced0
olmbde <olmbde@gmail.com>
2026-03-12 18:07:58 +0000

    Update supabase.ts

--- client/lib/supabase.ts
+++ client/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://ndnflifradbwcomcjpxa.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_kHqPFcfykrptmCBKwSDxgA_lWIUXQgb";
 
 if (!supabaseUrl || !supabaseKey) {
   console.warn(
